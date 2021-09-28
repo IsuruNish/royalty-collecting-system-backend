510d7d3bf3393cf68b9cb22c8d1d91f5d1c95c00
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-09-28 12:05:53 +0530

    Add the ShowOrganizer Class for Signup

---
+++ OSCA/src/main/java/org/osca/model/ShowOrganizer.java
@@ -0,0 +1,117 @@
+package org.osca.model;
+
+public class ShowOrganizer {
+    private int uid;
+    private String fname;
+    private String lname;
+    private String nic;
+    private String phone;
+    private String email;
+    private String password;
+    private String deleteStatus;
+    private String userType;
+    private String deletedBy;
+    private String deletedOn;
+
+
+
+    public ShowOrganizer() {
+    }
+
+    public ShowOrganizer(String fname, String lname, String nic, String phone, String email, String password) {
+        this.fname = fname;
+        this.lname = lname;
+        this.nic = nic;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDeleteStatus() {
+        return deleteStatus;
+    }
+
+    public void setDeleteStatus(String deleteStatus) {
+        this.deleteStatus = deleteStatus;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(String deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+
+    public String getDeletedOn() {
+        return deletedOn;
+    }
+
+    public void setDeletedOn(String deletedOn) {
+        this.deletedOn = deletedOn;
+    }
+}
