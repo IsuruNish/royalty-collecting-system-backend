414e10b511627600004b5eabfa6a26653e09fbe8
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-09-28 12:06:36 +0530

    Add the SignupService Class

---
+++ OSCA/src/main/java/org/osca/service/signupService.java
@@ -0,0 +1,19 @@
+package org.osca.service;
+
+import org.osca.dao.signupDAO;
+import org.osca.dao.signupDAOimp;
+import org.osca.model.ShowOrganizer;
+
+import java.sql.SQLException;
+
+public class signupService {
+    private signupDAO userDAO;
+
+    public signupService(){
+        userDAO = new signupDAOimp();
+    }
+
+    public boolean addShowOrganizers(ShowOrganizer ppl) throws SQLException, ClassNotFoundException {
+        return userDAO.addSOs(ppl);
+    }
+}
