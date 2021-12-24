5c48889a161b8ae7711daab33dd87d62c0cb8161
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-12-24 16:25:18 +0530

    Modify Cancel License Files

--- OSCA/src/main/java/org/osca/controller/SOCancelLicenseServlet.java
+++ OSCA/src/main/java/org/osca/controller/SOCancelLicenseServlet.java
@@ -1,132 +1,132 @@
-//package org.osca.controller;
-//
-//import com.google.gson.Gson;
-//import org.osca.controller.auth.JWebToken;
-//import org.osca.controller.httpRequest.HeaderAndBody;
-//import org.osca.model.*;
-//import org.osca.service.ImageService;
-//import org.osca.service.SOCancelLicenseService;
-//import org.osca.service.ShowOrganizerService;
-//
-//import javax.servlet.*;
-//import javax.servlet.http.*;
-//import javax.servlet.annotation.*;
-//import java.io.IOException;
-//import java.security.NoSuchAlgorithmException;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//
-//@WebServlet(name = "SOCancelLicenseServlet", value = "/SOCancelLicenseServlet")
-//public class SOCancelLicenseServlet extends HttpServlet {
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        HeaderAndBody data = new HeaderAndBody();
-//        String header = data.getAuthenticationHeader(request);
-//        String token = header.substring(7);
-//
-//        JWebToken tokennObj = null;
-//        try {
-//            tokennObj = new JWebToken(token);
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        }
-//        assert tokennObj != null;
-//        int uid = tokennObj.getUserID(token);
-//        int userType = tokennObj.getUserType(token);
-//        //int uid = 10001;
-//        SOCancelLicenseService service = new SOCancelLicenseService();
-//        ArrayList<ArrayList<String>> details = new ArrayList<>();
-//        try {
-//            details = service.getConcertDetails(uid);
-////            System.out.println(details);
-//
-//        } catch (SQLException throwables) {
-//            throwables.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        ShowOrganizerService SOserivice=new ShowOrganizerService();
-//        String fName=null;
-//        try {
-//             fName = SOserivice.getDashboardDetails(uid).get(1);
-//            System.out.println(fName);
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        ImageService dp = new ImageService();
-//        String path = null;
-//        try {
-//            path = dp.getUserDP(uid);
-//        } catch (SQLException throwables) {
-//            throwables.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//        SOCancelLicense cl = new SOCancelLicense(
-//                details.get(0),
-//                details.get(1),
-//                details.get(2),
-//                details.get(3),
-//                details.get(4),
-//                details.get(5));
-//
-//
-//
-//
-//        Gson gson = new Gson();
-//        System.out.println(cl);
-//        cl.setUtype(userType);
-//        cl.setDpPath(path);
-//        cl.setFname(fName);
-//        String saobj =gson.toJson(cl);
-//        System.out.println(saobj);
-//        response.setContentType("application/json");
-//        response.setCharacterEncoding("UTF-8");
-//        response.getWriter().println(saobj);
-//
-//
-//
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        System.out.println("hi");
-//
-//        HeaderAndBody data = new HeaderAndBody();
-//        String header = data.getAuthenticationHeader(request);
-//        String body = data.getBody(request);
-//        String token = header.substring(7);
-//        System.out.println(body);
-//        JWebToken tokennObj = null;
-//        try {
-//            tokennObj = new JWebToken(token);
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        }
-//        int uid = tokennObj.getUserID(token);
-//
-//
-//        ConcertIDModel concertIDModel;
-//        Gson gson = new Gson();
-//       concertIDModel = gson.fromJson(body, ConcertIDModel.class);
-//        SOCancelLicenseService service = new SOCancelLicenseService();
-//
-//        try {
-//            service.getID(concertIDModel);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//        response.setContentType("application/json");
-//        response.setCharacterEncoding("UTF-8");
-//
-//
-//    }
-//}
+package org.osca.controller;
+
+import com.google.gson.Gson;
+import org.osca.controller.auth.JWebToken;
+import org.osca.controller.httpRequest.HeaderAndBody;
+import org.osca.model.*;
+import org.osca.service.ImageService;
+import org.osca.service.SOCancelLicenseService;
+import org.osca.service.ShowOrganizerService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@WebServlet(name = "SOCancelLicenseServlet", value = "/SOCancelLicenseServlet")
+public class SOCancelLicenseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HeaderAndBody data = new HeaderAndBody();
+        String header = data.getAuthenticationHeader(request);
+        String token = header.substring(7);
+
+        JWebToken tokennObj = null;
+        try {
+            tokennObj = new JWebToken(token);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        assert tokennObj != null;
+        int uid = tokennObj.getUserID(token);
+        int userType = tokennObj.getUserType(token);
+        //int uid = 10001;
+        SOCancelLicenseService service = new SOCancelLicenseService();
+        ArrayList<ArrayList<String>> details = new ArrayList<>();
+        try {
+            details = service.getConcertDetails(uid);
+//            System.out.println(details);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        ShowOrganizerService SOserivice=new ShowOrganizerService();
+        String fName=null;
+        try {
+             fName = SOserivice.getDashboardDetails(uid).get(1);
+            System.out.println(fName);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        ImageService dp = new ImageService();
+        String path = null;
+        try {
+            path = dp.getUserDP(uid);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        SOCancelLicense cl = new SOCancelLicense(
+                details.get(0),
+                details.get(1),
+                details.get(2),
+                details.get(3),
+                details.get(4),
+                details.get(5));
+
+
+
+
+        Gson gson = new Gson();
+        System.out.println(cl);
+        cl.setUtype(userType);
+        cl.setDpPath(path);
+        cl.setFname(fName);
+        String saobj =gson.toJson(cl);
+        System.out.println(saobj);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().println(saobj);
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("hi");
+
+        HeaderAndBody data = new HeaderAndBody();
+        String header = data.getAuthenticationHeader(request);
+        String body = data.getBody(request);
+        String token = header.substring(7);
+        System.out.println(body);
+        JWebToken tokennObj = null;
+        try {
+            tokennObj = new JWebToken(token);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        int uid = tokennObj.getUserID(token);
+
+
+        ConcertIDModel concertIDModel;
+        Gson gson = new Gson();
+       concertIDModel = gson.fromJson(body, ConcertIDModel.class);
+        SOCancelLicenseService service = new SOCancelLicenseService();
+
+        try {
+            service.getID(concertIDModel);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+
+    }
+}
--- OSCA/src/main/java/org/osca/service/SOCancelLicenseService.java
+++ OSCA/src/main/java/org/osca/service/SOCancelLicenseService.java
@@ -1,30 +1,30 @@
-//package org.osca.service;
-//
-//import org.osca.dao.SOCancelLicenseDAO;
-//import org.osca.dao.SOCancelLicenseDAOImpl;
-//import org.osca.model.CancelLicense;
-//import org.osca.model.ConcertIDModel;
-//import org.osca.model.SOCancelLicense;
-//
-//
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//
-//public class SOCancelLicenseService {
-//
-//    private SOCancelLicenseDAO soCancelLicenseDAO;
-//
-//    public SOCancelLicenseService(){
-//        soCancelLicenseDAO=new SOCancelLicenseDAOImpl();
-//    }
-//
-//    public boolean getID(ConcertIDModel concertIDModel) throws SQLException, ClassNotFoundException {
-//        return soCancelLicenseDAO.getIDs(concertIDModel);
-////        return loginDAO.getUserID(userLoginModel);
-//    }
-//
-//    public ArrayList<ArrayList<String>> getConcertDetails(int uid) throws SQLException, ClassNotFoundException {
-//        return soCancelLicenseDAO.getDetails(uid);
-//    }
-//
-//}
+package org.osca.service;
+
+import org.osca.dao.SOCancelLicenseDAO;
+import org.osca.dao.SOCancelLicenseDAOImpl;
+import org.osca.model.CancelLicense;
+import org.osca.model.ConcertIDModel;
+import org.osca.model.SOCancelLicense;
+
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class SOCancelLicenseService {
+
+    private SOCancelLicenseDAO soCancelLicenseDAO;
+
+    public SOCancelLicenseService(){
+        soCancelLicenseDAO=new SOCancelLicenseDAOImpl();
+    }
+
+    public boolean getID(ConcertIDModel concertIDModel) throws SQLException, ClassNotFoundException {
+        return soCancelLicenseDAO.getIDs(concertIDModel);
+//        return loginDAO.getUserID(userLoginModel);
+    }
+
+    public ArrayList<ArrayList<String>> getConcertDetails(int uid) throws SQLException, ClassNotFoundException {
+        return soCancelLicenseDAO.getDetails(uid);
+    }
+
+}
